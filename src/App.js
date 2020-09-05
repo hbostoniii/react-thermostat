22eb55dcd1b17d76136a5d02dddc1aba7a5f1de2
Horace Boston <hbostoniii@gmail.com>
2020-09-05 00:07:03 -0500

    first commit, simple counter, added functionality for changing units, change color based on temperature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [temp, setTemp] = useState(68);
+	const [units, setUnits] = useState("ºF");
+
+	const celsiusToFahrenheit = () => {
+		setUnits("ºF");
+		setTemp(+((temp / 5) * 9 + 32).toFixed(0));
+	};
+
+	const fahrenheitToCelsius = () => {
+		setUnits("ºC");
+		setTemp(+(((temp - 32) * 5) / 9).toFixed(0));
+	};
+
+	const fahrenheitStyles = () => {
+		if (temp >= 86) {
+			return { color: "red" };
+		} else if (temp <= 50) {
+			return { color: "blue" };
+		}
+		return { color: "black" };
+	};
+
+	const celsiusStyles = () => {
+		if (temp >= 30) {
+			return { color: "red" };
+		} else if (temp <= 10) {
+			return { color: "blue" };
+		}
+		return { color: "black" };
+	};
+
+	return (
+		<div className="App">
+			<h1 style={units === "ºF" ? fahrenheitStyles() : celsiusStyles()}>
+				{temp}
+				{units} <br />
+				<span style={{ fontSize: "2rem", color: "gray" }}>
+					(temperature rounded to nearest integer)
+				</span>
+			</h1>
+			<div className="btn-wrapper">
+				<button onClick={() => setTemp(temp - 1)} className="tempChange">
+					-
+				</button>
+				<button
+					onClick={units === "ºF" ? fahrenheitToCelsius : celsiusToFahrenheit}
+					className="unitChange">
+					Change units
+				</button>
+				<button onClick={() => setTemp(temp + 1)} className="tempChange">
+					+
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
